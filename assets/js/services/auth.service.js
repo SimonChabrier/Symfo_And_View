afc83938e4d2d49ec0f64347c923441898339f01
Simon Chabrier <simonchabrier@gmail.com>
2023-08-09 12:03:38 +0200

    build

--- assets/js/services/auth.service.js
+++ assets/js/services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'https://127.0.0.1:8000/api/login';
-//const API_URL = 'https://vue-users.simschab.fr/api/login';
+//const API_URL = 'https://127.0.0.1:8000/api/login';
+const API_URL = 'https://vueusers.simschab.cloud/login';
 
 const authServices =  {
 
@@ -29,8 +29,8 @@ async getAuth(user) {
 async killAuth() {
     console.log('killAuth');
 
-    return await axios.post('https://127.0.0.1:8000/logout').then(response => {
-    //return await axios.post('https://vue-users.simschab.fr/logout').then(response => {
+    //return await axios.post('https://127.0.0.1:8000/logout').then(response => {
+    return await axios.post('https://vueusers.simschab.cloud/logout').then(response => {
 
         console.log(response.status);
 
